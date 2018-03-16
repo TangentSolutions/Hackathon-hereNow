a5693ad7288b4cc06b5be77774aa65d2f4cabd91
Hermann <hermann@tangentsolutions.co.za>
2018-03-16 12:39:39 +0200

    Added serializers

--- HereNow/urls.py
+++ HereNow/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
+from api.views import UserViewSet
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('user/', UserViewSet),
     url(r'^api-auth/', include('rest_framework.urls'))
 ]

7c4b946ef47fdfdc65f8d632fb11b517a2279e85
Paul Imisi <pimisi@yahoo.com>
2018-03-16 16:52:30 +0200

    Fixed location user issue

--- api/models.py
+++ api/models.py
@@ -1,51 +1,24 @@
+from django.contrib.auth.models import User
 from django.db import models
+from account.models import UserDetail
 
-
-class User(models.Model):
-    def __unicode__(self):
-        return self.username
-    
-    username = models.CharField(max_length=255, unique=True, db_index=True)
-    name = models.CharField(max_length=255)
-    email = models.CharField(max_length=255)
-    address = models.TextField(null=True, blank=True, default=None)
-    phone = models.TextField(null=True, blank=True, default=None)
-    password = models.TextField(max_length=255)
-    related_users = models.ForeignKey('User', on_delete=models.CASCADE, related_name="user_related_users", null=True, blank=True)
-    created = models.DateTimeField(auto_now_add=True)
-    role = models.CharField(max_length=255)
-    token = models.CharField(max_length=255, null=True, blank=True, default=None)
-    last_modified = models.DateTimeField(auto_now=True)
-
-
-
-# class UserRole(models.Model):
-#     name = models.CharField(max_length=50)
-#     description = models.CharField(max_length=2000)
-
-
-# class User(models.Model):
-#     user = models.OneToOneField(settings.Auth)
 
 class GeoFencing(models.Model):
-    def __unicode__(self):
-        return self.username
-    
     lon = models.FloatField()
     lat = models.FloatField()
     radius = models.IntegerField()
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
-    user = models.ForeignKey('User', on_delete=models.CASCADE, related_name="geo_fencing_user")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="geo_fencing_user")
 
 
 class Location(models.Model):
     def __unicode__(self):
-        return "({0}, {1})".format(self.lat, self.lon)
+        return "({0}, {1})".format(self.lat, self.long)
     
     long = models.FloatField()
     lat = models.FloatField()
     time = models.DateTimeField()
     is_safe = models.NullBooleanField (null=True, blank=True, default=None)
-    user = models.ForeignKey('User', on_delete=models.CASCADE, related_name="location_user")
+    user = models.ManyToManyField(User, related_name="location_user")
 

a8fbb32907b371b4f8666f762297855e93bddccd
Kgaugelo Bopape <kgaugelo20@gmail.com>
2018-03-16 11:47:37 +0200

    added root url

--- api/views.py
+++ api/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+class GeofenceViewSet():
+
+    def create(request):
+        return HttpResponse(request)

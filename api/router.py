02e8537482e7a048a905583570566ce68c3142fd
Paul Imisi <pimisi@yahoo.com>
2018-03-16 12:52:45 +0200

    Added Location Serializer, ViewSet and route

--- api/router.py
+++ api/router.py
@@ -1,6 +1,12 @@
 from django.conf.urls import url, include
 from rest_framework import routers
 
+from api.views import LocationViewSet
+
 router = routers.SimpleRouter()
 
-urlpatterns = []
+router.register(r'locations', LocationViewSet)
+
+urlpatterns = [
+    url(r'^', include(router.urls))
+]

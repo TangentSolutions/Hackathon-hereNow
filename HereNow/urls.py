b38edecc9fd4bdbc22a9ad7ce342e6fbaea4b611
Paul Imisi <pimisi@yahoo.com>
2018-03-16 12:30:25 +0200

    Added api roiuter

--- HereNow/urls.py
+++ HereNow/urls.py
@@ -17,7 +17,11 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.urls import path
 
+from api import router
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^api-auth/', include('rest_framework.urls'))
+    url(r'^api-auth/', include('rest_framework.urls')),
+    url(r'api/', include(router))
 ]
